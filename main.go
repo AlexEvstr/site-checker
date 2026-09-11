2f0c4427a7a5820716a162e9e0c9251d76c593be
Alexander Evstratov <alexdrugev@me.com>
2026-09-11 13:18:40 +0300

    feat: add URL check function

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/http"
 	"time"
 )
 
@@ -9,4 +10,25 @@ type CheckResult struct {
 	StatusCode int
 	Duration   time.Duration
 	Err        error
+}
+
+func checkURL(url string) CheckResult {
+	start := time.Now()
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+	resp, err := client.Get(url)
+	if err != nil {
+		return CheckResult{
+			URL:      url,
+			Duration: time.Since(start),
+			Err:      err,
+		}
+	}
+	defer resp.Body.Close()
+	return CheckResult{
+		URL:        url,
+		StatusCode: resp.StatusCode,
+		Duration:   time.Since(start),
+	}
 }
